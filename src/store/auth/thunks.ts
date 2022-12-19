0a1e3ed5861a46c567dec328f34fe2db37b9b81e
TomasDep <tomas.valenzuelacb@gmail.com>
2022-12-18 21:14:24 -0300

    se agrega logueo con google y correo y contraseña

--- src/store/auth/thunks.ts
+++ src/store/auth/thunks.ts
@@ -1,13 +1,54 @@
-import { checkingCredentials } from "./authSlice";
+import { checkingCredentials, login, logout } from "./authSlice";
+import {
+  loginWithEmailAndPassword,
+  signInWithGoogle,
+  registerUserWithEmailPassword,
+} from "../../firebase/providers";
+import { AnyAction, ThunkAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
 
-export const checkingAuthentication = (email: string, password: string) => {
-  return async (dispatch: any) => {
-    dispatch(checkingCredentials());
-  };
+export const checkingAuthentication = () => async (dispatch: any) => {
+  dispatch(checkingCredentials());
 };
 
-export const startGoogleSignIn = () => {
-  return async (dispatch: any) => {
+export const startGoogleSignIn =
+  (): ThunkAction<void, RootState, unknown, AnyAction> => async (dispatch) => {
     dispatch(checkingCredentials());
+    const result = await signInWithGoogle();
+    if (!result.status) dispatch(logout(result.message));
+    dispatch(login(result));
   };
-};
+
+export const startCreatingUserWithEmailAndPassword =
+  (
+    email: string,
+    password: string,
+    displayName: string
+  ): ThunkAction<void, RootState, unknown, AnyAction> =>
+  async (dispatch) => {
+    dispatch(checkingCredentials());
+    const { status, uid, photoURL, message } =
+      await registerUserWithEmailPassword({
+        email,
+        password,
+        displayName,
+      });
+    if (!status) return dispatch(logout({ message }));
+    dispatch(login({ user: { uid, displayName, email, photoURL } }));
+  };
+
+export const startLoginWithEmailAndPassword =
+  (
+    email: string,
+    password: string
+  ): ThunkAction<void, RootState, unknown, AnyAction> =>
+  async (dispatch) => {
+    dispatch(checkingAuthentication());
+    const { uid, displayName, photoURL, status, message } =
+      await loginWithEmailAndPassword({
+        email,
+        password,
+      });
+    if (!status) return dispatch(logout({ message }));
+    dispatch(login({ user: { uid, displayName, email, photoURL } }));
+  };
