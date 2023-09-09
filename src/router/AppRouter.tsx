30211fcaea3422d658f2a976f52e80f5b6846ebe
TomasDep <tomas.valenzuelacb@gmail.com>
2023-09-08 22:31:28 -0300

    implement hook checkAuth

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,13 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import { AuthRoutes } from "../auth";
 import { JournalRoutes } from "../journal";
+import { CheckingAuth } from "../ui";
+import { useCheckAuth } from "../hooks";
 
 export const AppRouter = () => {
+  const { status } = useCheckAuth();
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
   return (
     <Routes>
-      <Route path="/auth/*" element={<AuthRoutes />} />
-      <Route path="/*" element={<JournalRoutes />} />
+      {status === "AUTHENTICATED" ? (
+        <Route path="/*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      )}
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   );
 };
