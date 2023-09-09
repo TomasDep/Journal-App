16d588674295e68e0b8791e068a0447b4ca19a03
Tomas Valenzuela (agilesoft) <tvalenzuela@agilesoft.cl>
2023-09-08 22:27:30 -0300

    ADD Animate.css

--- src/journal/layout/JournalLayout.tsx
+++ src/journal/layout/JournalLayout.tsx
@@ -1,13 +1,22 @@
+import { FC } from "react";
+
 import { Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
 
 import { NavBar, Sidebar } from "../components";
 
+interface Props {
+  children: any;
+}
+
 const drawerWidth = 280;
 
-export const JournalLayout = ({ children }: { children: any }) => {
+export const JournalLayout: FC<Props> = ({ children }) => {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box
+      sx={{ display: "flex" }}
+      className="animate__animated animate__fadeIn animate__faster"
+    >
       <NavBar drawerWidth={drawerWidth} />
       <Sidebar drawerWidth={drawerWidth} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
