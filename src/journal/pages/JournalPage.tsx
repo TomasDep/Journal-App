1a73f82ba39f6f760e3c4f95ea798cbdad84e935
TomasDep <tomas.valenzuelacb@gmail.com>
2023-09-08 22:31:00 -0300

    remove comment

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,18 +1,12 @@
-import { IconButton, Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
 import AddOutlined from "@mui/icons-material/AddOutlined";
 
 import { JournalLayout } from "../layout";
-import { NoteView, NothingSelectedView } from "../view";
+import { NothingSelectedView } from "../view";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <Typography>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptate
-        beatae officiis repudiandae ad architecto saepe magni, ea quidem
-        deleniti natus exercitationem, sit inventore doloremque cupiditate,
-        minus quae omnis unde!
-      </Typography> */}
       <NothingSelectedView />
       {/* <NoteView /> */}
       <IconButton
