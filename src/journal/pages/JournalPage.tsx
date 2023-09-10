c0e30e4c8ff8f764fb8c643ebbb047534f04a824
TomasDep <tomas.valenzuelacb@gmail.com>
2023-09-09 22:49:59 -0300

    ADD funcion for create new note

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,16 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+
 import { IconButton } from "@mui/material";
 import AddOutlined from "@mui/icons-material/AddOutlined";
 
 import { JournalLayout } from "../layout";
-import { NothingSelectedView } from "../view";
+import { NoteView, NothingSelectedView } from "../view";
+import { startNewNote } from "../../store/journal";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+  const { isSaving, active } = useSelector((state: any) => state.journal);
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  };
   return (
     <JournalLayout>
-      <NothingSelectedView />
-      {/* <NoteView /> */}
+      {!!active ? <NoteView /> : <NothingSelectedView />}
+
       <IconButton
+        onClick={onClickNewNote}
         size="large"
+        disabled={isSaving}
         sx={{
           color: "#fff",
           bgcolor: "error.main",
