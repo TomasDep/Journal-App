3f904d033371b65d27c9dae17e763c88ad8ede48
TomasDep <tomas.valenzuelacb@gmail.com>
2023-09-09 22:48:49 -0300

    ADD loading notes

--- src/hooks/useCheckAuth.ts
+++ src/hooks/useCheckAuth.ts
@@ -5,15 +5,17 @@ import { onAuthStateChanged } from "firebase/auth";
 import { FirebaseAuth } from "../firebase/config";
 
 import { login, logout } from "../store/auth";
+import { startLoadingNotes } from "../store/journal";
 
 export const useCheckAuth = () => {
   const { status } = useSelector((state: any) => state.auth);
   const dispatch = useDispatch();
   useEffect(() => {
     onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout());
+      if (!user) return dispatch(logout({}));
       const { uid, email, displayName, photoURL } = user;
       dispatch(login({ user: { uid, displayName, email, photoURL } }));
+      dispatch(startLoadingNotes());
     });
   }, []);
   return {
